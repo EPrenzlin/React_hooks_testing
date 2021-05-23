3683b8fe425c7306f3447c7bd25ffc37c15b2e74
github username <eprenzlin@gmail.com>
2021-05-23 15:43:27 +0100

    adding in like button for each quote

--- src/containers/quotes.js
+++ src/containers/quotes.js
@@ -1,11 +1,29 @@
 import Card from 'react-bootstrap/Card'
-
 import { Share } from 'react-twitter-widgets'
- 
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
 
 import React from 'react'
 
 class Quotes extends React.Component{
+
+likeQuote = () => {
+    
+fetch('https://example.com/profile', {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(this.props.quote),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
+}
 
 render(){
     return(
@@ -13,11 +31,14 @@ render(){
            <Card.Body text ="primary">
             <p> 
             <h4 style={{textAlign :"center"}}> {this.props.quote} </h4>
+            <Container> 
             <Share url={this.props.quote} />
+            <Button variant="outline-primary" active onClick={this.likeQuote}>Like</Button>{' '}
+            </Container>
             </p>
            </Card.Body>
        </Card>
-    )
+        )
     }
 
 }
